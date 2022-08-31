4e06cfded0635c1e18a724f287f865a3fe835c52
basicasian <nana.zhang.00@gmail.com>
2022-08-31 23:05:01 +0200

    implemented body controlled steering and added more objects to scenes

---
+++ Assets/Scripts/BodyBasedSteering.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Unity.XR.CoreUtils;
+
+public class BodyBasedSteering : MonoBehaviour
+{
+    public InputActionReference steeringReference = null;
+    public Camera mainCamera = null;
+    public XROrigin xrOrigin = null;
+
+    public float speed = 0;
+
+    private void Update()
+    {
+        if (steeringReference.action.IsPressed())
+        {
+            Steering();
+        }
+    }
+
+    private void Steering()
+    {
+        Vector3 deltaSteering = (Vector3.Scale(mainCamera.transform.forward, new Vector3(1.0f, 0.0f, 1.0f))); 
+        xrOrigin.transform.position += deltaSteering * speed * Time.deltaTime;
+    }
+}
